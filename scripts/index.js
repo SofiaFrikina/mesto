fdd501856dc2803dbb07072b183097cba4600f12
SofiaFrikina <sofi.frikina@yandex.ru>
2023-04-18 20:34:04 +0300

    До исправлений в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let popUp = document.querySelector('.popup');
-let popUpBody = popUp.querySelector('.popup__body');
+let popUpContainer = popUp.querySelector('.popup__container');
 let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
 let popUpClose = popUp.querySelector('.popup__close');
@@ -8,28 +8,21 @@ let popUpButton = popUp.querySelector('.popup__button');
 editButton.addEventListener('click', function (e) {
     e.preventDefault();
     popUp.classList.add('active');
-    popUpBody.classList.add('active');
+    popUpContainer.classList.add('active');
 });
 
 popUpClose.addEventListener('click', () => {
     popUp.classList.remove('active');
-    popUpBody.classList.remove('active');
+    popUpContainer.classList.remove('active');
 });
-
-
-popUpButton.addEventListener('click', () => {
-    popUp.classList.remove('active');
-    popUpBody.classList.remove('active');
-});
-
 
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     let profileTitle = profile.querySelector('.profile__title');
     let profileSubTitle = profile.querySelector('.profile__subtitle');
-    let nameInput = popUp.querySelector('.popup__name');
-    let jobInput = popUp.querySelector('.popup__job');
+    let nameInput = popUp.querySelector('.popup__input_type_name');
+    let jobInput = popUp.querySelector('.popup__input_type_job');
 
 
     profileTitle.textContent = nameInput.value;
