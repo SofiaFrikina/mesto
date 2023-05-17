9bd30f9159afd08105cd545d93577e0f4835c2b3
SofiaFrikina <sofi.frikina@yandex.ru>
2023-05-17 16:19:54 +0300

    1 отправка на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -14,9 +14,13 @@ const addButton = profile.querySelector('.profile__add-button');
 const profileTitle = profile.querySelector('.profile__title');
 const profileSubTitle = profile.querySelector('.profile__subtitle');
 
+const form = document.querySelector('.popup__form');
+const formInput = form.querySelector('.popup__input');
+const formError = form.querySelector(`.${formInput.id}-error`)
 const popUpForm = document.querySelector('.popup__form_type_one');
 const nameInput = popUpForm.querySelector('.popup__input_type_name');
 const jobInput = popUpForm.querySelector('.popup__input_type_job');
+
 
 const popUpTwoForm = document.querySelector('.popup__form_type_two');
 const textInput = popUpTwoForm.querySelector('.popup__input_type_text');
@@ -55,6 +59,99 @@ function handleFormSubmit(evt) {
 
 popUpButton.addEventListener('click', handleFormSubmit);
 popUpForm.addEventListener('submit', handleFormSubmit);
+
+//создаем функцию, которая будет показывать ошибку, если введенный код невалиден
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_disabled');//красная подстрока
+    //inputElement.setAttribute('disabled', true);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__text-error_active');
+
+}
+//создаем функцию, которая будет прятать ошибку, если введенный код валиден
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_disabled');
+    //inputElement.removeAttribute('disabled', true);
+    errorElement.textContent = '';
+    errorElement.classList.remove('popup__text-error_active');
+
+}
+
+//создаем функцию, которая будет проверять валиден ли код
+const isValid = (formElement, inputElement) => {
+    if (!formInput.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+}
+
+
+
+//создаем функцию, которая позволит слушателю событий добавиться всем полям инпут внутри формы
+const eventListener = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+    toggleButton(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+        // каждому полю добавим обработчик события input
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement);
+            toggleButton(inputList, buttonElement);
+        });
+    });
+}
+
+const hasInvalidInput = (inputList) => {
+    // проходим массиву
+    return inputList.some((inputElement) => {
+        //если поле не валидно колбэк вернёт true
+        return !inputElement.validity.valid;
+    });
+};
+
+//создаем функцию, которая принимает массив полей ввода и кнопку
+const toggleButton = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        //кнопка неактивная
+        buttonElement.classList.add('popup__button_disabled');
+        buttonElement.setAttribute('disabled', true);
+    } else {
+        //кнопка активная
+        buttonElement.classList.remove('popup__button_disabled');
+        buttonElement.removeAttribute('disabled', true);
+    }
+};
+
+//создаем функцию, которая найдёт и переберёт все формы на странице
+const everyForm = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+    formList.forEach((formElement) => {
+        eventListener(formElement);
+    });
+}
+
+everyForm();
+
+//закрытие попапов по оверлею и escape
+document.querySelectorAll('.popup').forEach((element) => {
+    const newPopUp = element.closest('.popup');
+    document.addEventListener('keydown', function (event) {
+        const key = event.key; // const {key} = event; in ES6+
+        if (key === "Escape") {
+            closePopUp(newPopUp);
+        }
+    });
+    element.addEventListener("click", (evt) => {
+        if (evt.currentTarget === evt.target) {
+            closePopUp(newPopUp)
+        }
+    });
+});
+
+
 
 const elementsList = document.querySelector('.elements');
 const elementsTemplate = document.querySelector('.elements-template').content;
